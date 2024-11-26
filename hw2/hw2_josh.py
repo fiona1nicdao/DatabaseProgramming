8a41f55371477efdde9a5963249d78ee02c710a9
FionaN <fiona1941nicdao@gmail.com>
2024-11-25 20:30:16 -0600

    add changes to Josh queries

--- hw2/hw2_josh.py
+++ hw2/hw2_josh.py
@@ -1,13 +1,16 @@
-import pymongo
+
+import pymongo 
 from pymongo import MongoClient
 import pdb
 from tabulate import tabulate
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 client = MongoClient(
-    f"mongodb+srv://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASS')}@453cluster.zy6th.mongodb.net/?retryWrites=true&w=majority&appName=453cluster"
+    "mongodb+srv://lucuser:csclassluc@luccluster.k4by9.mongodb.net/?retryWrites=true&w=majority&appName=LUCcluster"
+
+    # f"mongodb+srv://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASS')}@453cluster.zy6th.mongodb.net/?retryWrites=true&w=majority&appName=453cluster"
 )
 db = client.sample_airbnb
 listings = db.listingsAndReviews
@@ -39,22 +42,14 @@ print(tabulate(q2Results, headers=q2Headers), "\n")
 # Question 3
 print("Question 3")
 q3Query = {
-    "$match": {
-        "$expr": {
-            "$gt": [
-                "$review_scores.review_scores_cleanliness",
-                "$review_scores.review_scores_accuracy",
-            ]
-        }
+    "$expr": {
+        "$gt": [
+            "$review_scores.review_scores_cleanliness",
+            "$review_scores.review_scores_accuracy",
+        ]
     }
 }
-q3 = listings.aggregate(
-    [
-        q3Query,
-        {"$sort": {"number_of_reviews": pymongo.ASCENDING}},
-        {"$limit": 5},
-    ]
-)
+q3 = listings.find(q3Query).sort("number_of_reviews", pymongo.ASCENDING).limit(5)
 q3Headers, q3Results = [
     "_id",
     "name",
@@ -76,16 +71,12 @@ print(tabulate(q3Results, headers=q3Headers), "\n")
 
 # Question 4
 print("Question 4")
-q4Query = {
-    "$match": {"$and": [{"address.country_code": "PT"}, {"bedrooms": {"$gte": 3}}]}
-}
-q4Sort = {
-    "$sort": {
-        "review_scores.review_scores_rating": pymongo.DESCENDING,
-        "number_of_reviews": pymongo.DESCENDING,
-    }
-}
-q4 = listings.aggregate([q4Query, q4Sort, {"$limit": 5}])
+q4Query = {"$and": [{"address.country_code": "PT"}, {"bedrooms": {"$gte": 3}}]}
+q4Sort = [
+    ("review_scores.review_scores_rating", pymongo.DESCENDING),
+    ("number_of_reviews", pymongo.DESCENDING),
+]
+q4 = listings.find(q4Query).sort(q4Sort).limit(5)
 q4Headers, q4Results = [
     "_id",
     "name",
@@ -110,20 +101,16 @@ print(tabulate(q4Results, headers=q4Headers), "\n")
 # Question 5
 print("Question 5")
 q5Query = {
-    "$match": {
-        "$and": [
-            {"number_of_reviews": {"$lt": 500}},
-            {"review_scores.review_scores_communication": {"$gte": 9}},
-        ]
-    }
+    "$and": [
+        {"number_of_reviews": {"$lt": 500}},
+        {"review_scores.review_scores_communication": {"$gte": 9}},
+    ]
 }
-q5Sort = {
-    "$sort": {
-        "review_scores.review_score_checkin": pymongo.DESCENDING,
-        "number_of_reviews": pymongo.DESCENDING,
-    }
-}
-q5 = listings.aggregate([q5Query, q5Sort, {"$limit": 5}])
+q5Sort = [
+    ("review_scores.review_scores_checkin", pymongo.DESCENDING),
+    ("number_of_reviews", pymongo.DESCENDING),
+]
+q5 = listings.find(q5Query).sort(q5Sort).limit(5)
 q5Headers, q5Results = [
     "_id",
     "name",
@@ -148,26 +135,17 @@ print("Question 6")
 q6Query = {"$match": {"property_type": "House"}}
 q6Group = {
     "$group": {
-        "_id": {
-            "host_id": "$host.host_id",
-            "host_name": "$host.host_name"
-        },
+        "_id": {"host_id": "$host.host_id", "host_name": "$host.host_name"},
         "total_bedrooms": {
             "$sum": "$bedrooms",
         },
     }
 }
-q6Sort = {
-    "$sort": {
-        "total_bedrooms": pymongo.DESCENDING
-    }
-}
+q6Sort = {"$sort": {"total_bedrooms": pymongo.DESCENDING}}
 q6 = listings.aggregate([q6Query, q6Group, q6Sort, {"$limit": 5}])
 q6Headers, q6Results = ["host_id", "host_name", "total_bedrooms"], []
 for result in q6:
-    q6Results.append([
-        result["_id"]["host_id"],
-        result["_id"]["host_name"],
-        result["total_bedrooms"]
-    ])
+    q6Results.append(
+        [result["_id"]["host_id"], result["_id"]["host_name"], result["total_bedrooms"]]
+    )
 print(tabulate(q6Results, headers=q6Headers), "\n")
